39d8fb90192b8c0ba70252eeb2fb30c153149cab
kawofong <14829553+kawofong@users.noreply.github.com>
2025-04-09 20:16:50 -0400

    chore: stub code placeholders

--- src/constants.py
+++ src/constants.py
@@ -2,4 +2,12 @@
 Module for defining constants.
 """
 
+from enum import StrEnum
+
 TASK_QUEUE_NAME = "hello-task-queue"
+
+
+class CustomerRewardLevel(StrEnum):
+    BASIC = "BASIC"
+    GOLD = "GOLD"
+    PLATINUM = "PLATINUM"
--- src/workflows.py
+++ src/workflows.py
@@ -9,12 +9,28 @@ from temporalio import workflow
 # Import activity, passing it through the sandbox without reloading the module
 with workflow.unsafe.imports_passed_through():
     from activities import say_hello
+    from constants import CustomerRewardLevel
 
 
 @workflow.defn
-class SayHello:
+class CustomerRewardAccount:
+    def __init__(self):
+        self.level: CustomerRewardLevel = CustomerRewardLevel.BASIC
+        self.points: int = 0
+        self.active: bool = True
+
     @workflow.run
     async def run(self, name: str) -> str:
+        # TODO(kawo): call some external APIs to confirm user exists
+
+        # TODO(kawo): while account is active, keep track of points
+
+        # TODO(kawo): create a handler to add points
+
+        # TODO(kawo): promote level based on points
+
+        # TODO(kawo): create a handler to leave program and complete workflow
+
         return await workflow.execute_activity(
             say_hello, name, start_to_close_timeout=timedelta(seconds=5)
         )
